8aeb1c2db995c0e6d0b99e8e792d987cefb989a5
caua <caua999999@gmail.com>
2026-06-23 22:22:20 -0300

    feat: usa hook useMessage no app com mensagem personalizada

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import "./global.css"
 
 import { Button } from "./components/button"
 
-import "./styles.css"
+import { useMessage } from "./hooks/useMessage"
+
+import styles from "./app.module.css"
+
+
 
 export function App() {
+  const { show } = useMessage({age: 18, name: "caua" })
   return (
-    <div className="container">
-      <Button name="Criar" onClick={() => alert("Criar")}/>
-      <Button name="Editar"/>
+    <div className={styles.container}>
+      <Button
+       name="Adicionar"
+        onClick={() => show("Mensagem personalizada do meu hook!")}/>
+      <span>0</span>
       <Button name="Remover"/>
     </div>
   )
